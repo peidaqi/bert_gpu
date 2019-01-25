1abbbf659c1ea3238f6e34a6daac6b20cd6748d3
peidaqi <peidaqi@gmail.com>
2019-01-25 17:05:03 -0500

    Updated Google BERT Tensorflow code for multi-GPU support. Use FLAGS.use_tpu to turn it on/off.
    Now it works by:
    1. Turning off warmup.
    2. Use the vanilla AdamOptimizer which supports tf.contrib.distribute.
    
    These are work-arounds and will be fixed later.

--- modeling.py
+++ modeling.py
@@ -26,6 +26,8 @@ import re
 import six
 import tensorflow as tf
 
+from common import FLAGS
+
 
 class BertConfig(object):
   """Configuration for `BertModel`."""
@@ -335,7 +337,11 @@ def get_assignment_map_from_checkpoint(tvars, init_checkpoint):
     (name, var) = (x[0], x[1])
     if name not in name_to_variable:
       continue
-    assignment_map[name] = name
+
+    if FLAGS.use_tpu:
+        assignment_map[name] = name
+    else:
+        assignment_map[name] = name_to_variable[name]
     initialized_variable_names[name] = 1
     initialized_variable_names[name + ":0"] = 1
 
